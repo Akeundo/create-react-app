b59501a3aa2f9392496aad2efb8e1f5b1e4c1884
Akeundo <nvakeundo@yahoo.fr>
2019-08-14 13:50:20 +0300

    lesson 2 react

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { component } from 'react';
+//import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
+import { checkPropTypes } from 'prop-types';
 
 //import * as serviceWorker from './serviceWorker';
 
 const customStyle = {
-    'color' : 'red',
-    'backgroundColor' : 'aqua',
+  color: 'red',
+  backgroundColor: 'aqua',
 };
 
 /*const helloMessage = React.createElement (
@@ -57,24 +59,174 @@ ReactDOM.render(
     // more codes missing
     */
 
+//using customised content
 
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div class="btn">
+        <h1>Hello from React Component</h1>
+      </div>
+    );
+  }
+}
 
-    //using customised content
+/*ReactDOM.render(
+       < HelloMessage/>,
+       document.getElementById('root')
+    );
 
-    class helloMessage extends React.Component {
+    class HelloMessage2 extends React.Component {
         render() {
+            console.log(this.props)
             return (
                 <div class="btn">
-                    <h1>Hello from React Component</h1>
+                    <h1>{this.props.msg}</h1>
+                    <h2>{this.props.msg2}</h2>
+                    
                 </div>
             )
         }
     }
 
+// creating a constant to hold the greeting messages
+
+
+const greetingMessages ={
+morning: 'Good morning',
+afternoon: 'Good afternoon',
+evening: 'Good evening'
+
+};
     ReactDOM.render(
-       < helloMessage/>,
-       document.getElementById('root')
+        < HelloMessage/>,
+        document.getElementById('root')
+     );
+ 
+     class HelloMessage2 extends React.Component {
+         render() {
+             console.log(this.props)
+             return (
+                 <div class="btn">
+                     <h1>{this.props.morningMessage}</h1>
+                     <h2>{this.props.eveningMessage}</h2>
+                     <h2>{this.props.afternoonMessage}</h2>
+                     
+                 </div>
+             )
+         }
+     }
+
+    ReactDOM.render(
+       < HelloMessage2
+       morningMessage={greetingMessages.morning}
+       afternoonMessage={greetingMessages.afternoon}
+       eveningMessage={greetingMessages.evening}
+        />,
+       document.getElementById('root2')
     );
+
+//Further example
+
+const greetingMessages ={
+    morning: 'Good morning',
+    afternoon: 'Good afternoon',
+    evening: 'Good evening'
+    
+    };
+    class HelloMessage2 extends React.Component {
+        fetchGreetingMessage = time => {
+            if (time <= 12) {
+                return this.props.morningMessage;
+            } else if (time <= 18) {
+                return this.props.afternoonMessage;
+            }else {
+                return this.props.eveningMessage;
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.fetchGreetingMessage.time}</h1>
+                <h1>The time now is {this.props.time}</h1>
+            </div>
+        )
+    }
+        
+    ReactDOM.render(
+        morningMessage={greetingMessages.morning}
+       afternoonMessage={greetingMessages.afternoon}
+       eveningMessage={greetingMessages.evening}
+        document.getElementById('root2')
+     );
+     
+        // using functions
+
+        function fetchGreetingMessage (props) {
+            if (time <= 12) {
+                return this.props.morningMessage;
+            } else if (time <= 18) {
+                return this.props.afternoonMessage;
+            }else {
+                return this.props.eveningMessage;
+
+        }
+
+        function HelloMessage(props) {
+            return <div>
+                <h1>{ fetchGreetingMessage(props, props.timeNow)}</h1>
+                <h1>The time now is {props.time}</h1>
+            </div>
+        }
+
+        render (
+            <HelloMessage
+            morningMessage={greetingMessages.morning}
+            afternoonMessage={greetingMessages.afternoon}
+            eveningMessage={greetingMessages.evening}
+            timeNow={hour}
+            today={now}/>,
+            document.getElementById('root2')
+        );
+*/
+// Exercises solutions
+const movieList = [
+  { title: 'transformer', description: 'amazing', staring: 'actor 1' },
+  { title: 'spiderman', description: 'wao', staring: 'actor 2' },
+  { title: 'Passion of Christ', description: 'passionate', staring: 'actor 3' },
+];
+
+function Movie(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <p>{props.staring}</p>
+    </div>
+  );
+}
+
+function Cinema(props) {
+  return (
+    <div>
+      {props.movies.map((movie, index) => {
+        //composing component
+        return (
+          <Movie
+            key={index}
+            title={movie.title}
+            description={movie.description}
+            staring={movie.staring}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+render(<Cinema movies={movieList} />, document.getElementById('root2'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
