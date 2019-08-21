6cf6dec224e989a1a635d794c602eb9237133dea
Akeundo <nvakeundo@yahoo.fr>
2019-08-21 14:21:43 +0300

    calculator app

---
+++ calculator/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import sayHello from './resources/utils';
+import Keys from './components/Keys';
+
+const style = {
+  padding: '20px 0',
+  backgroundColor: 'black',
+  width: '100%',
+  height: '100%',
+  fontSize: '30px',
+  color: 'white',
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: null,
+      triggerCalc: false,
+      output: '',
+      disableSymbol: false,
+    };
+
+    this.addInput = this.addInput.bind(this);
+    this.calculate = this.calculate.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  addInput(e) {
+    this.setState({
+      disableSymbol: isNaN(e.target.innerText),
+      output: this.state.output + e.target.innerText,
+    });
+  }
+
+  calculate() {
+    this.setState({
+      triggerCalc: true,
+      output:
+        this.state.output !== '' ? eval(this.state.output) : this.state.output,
+    });
+  }
+
+  reset() {
+    this.setState({
+      disableSymbol: false,
+      triggerCalc: false,
+      output: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="result" style={style}>
+          {this.state.output !== ''
+            ? this.state.output
+            : 'The output goes here'}
+        </div>
+        <Keys
+          addInput={this.addInput}
+          calculate={this.calculate}
+          reset={this.reset}
+          disableSymbol={this.state.disableSymbol}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ calculator/src/components/Keys.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Keys.css';
+
+class Keys extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        <button onClick={this.props.addInput}>1</button>
+        <button onClick={this.props.addInput}>2</button>
+        <button onClick={this.props.addInput}>3</button>
+        <button
+          class="symbolBtn"
+          disabled={this.props.disableSymbol ? 'disabled' : ''}
+          onClick={this.props.addInput}
+        >
+          +
+        </button>
+        <button onClick={this.props.addInput}>4</button>
+        <button onClick={this.props.addInput}>5</button>
+        <button onClick={this.props.addInput}>6</button>
+        <button
+          class="symbolBtn"
+          disabled={this.props.disableSymbol ? 'disabled' : ''}
+          onClick={this.props.addInput}
+        >
+          -
+        </button>
+        <button onClick={this.props.addInput}>7</button>
+        <button onClick={this.props.addInput}>8</button>
+        <button onClick={this.props.addInput}>9</button>
+        <button
+          class="symbolBtn"
+          disabled={this.props.disableSymbol ? 'disabled' : ''}
+          onClick={this.props.addInput}
+        >
+          *
+        </button>
+        <button onClick={this.props.addInput}>0</button>
+
+        <button
+          class="symbolBtn"
+          disabled={this.props.disableSymbol ? 'disabled' : ''}
+          onClick={this.props.addInput}
+        >
+          /
+        </button>
+        <button class="symbolBtn" onClick={this.props.reset}>
+          C
+        </button>
+        <button class="symbolBtn" onClick={this.props.calculate}>
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Keys;
